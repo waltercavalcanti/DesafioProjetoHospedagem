e20a973a0c48c8d54d194a22a3ed4972163fd049
Walter Cavalcanti <waltercavalcanti@gmail.com>
2025-05-20 16:14:50 -0300

    Desafio concluído!

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -1,18 +1,17 @@
-namespace DesafioProjetoHospedagem.Models
+namespace DesafioProjetoHospedagem.Models;
+
+public class Suite
 {
-    public class Suite
+    public Suite() { }
+
+    public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
     {
-        public Suite() { }
-
-        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
-        {
-            TipoSuite = tipoSuite;
-            Capacidade = capacidade;
-            ValorDiaria = valorDiaria;
-        }
-
-        public string TipoSuite { get; set; }
-        public int Capacidade { get; set; }
-        public decimal ValorDiaria { get; set; }
+        TipoSuite = tipoSuite;
+        Capacidade = capacidade;
+        ValorDiaria = valorDiaria;
     }
+
+    public string TipoSuite { get; set; }
+    public int Capacidade { get; set; }
+    public decimal ValorDiaria { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -6,17 +6,30 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Console.Write("Informe a quantidade de dias reservados: ");
+int diasReservados = Convert.ToInt32(Console.ReadLine());
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+Console.Write("Informe a quantidade de hóspedes: ");
+int quantidadeHospedes = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < quantidadeHospedes; i++)
+{
+    Console.WriteLine($"Hóspede {i + 1}");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Sobrenome: ");
+    string sobrenome = Console.ReadLine();
+
+    Pessoa hospede = new Pessoa(nome, sobrenome);
+
+    hospedes.Add(hospede);
+}
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 1, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
